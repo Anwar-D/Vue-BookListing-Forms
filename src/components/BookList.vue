61e9b25f80b6095ebc6834cf070dc0644b7b2110
Anwar <anyes202@hotmail.com>
2020-06-26 02:06:01 +0300

    task2-1

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h1>{{title}}</h1>
-    <ul>
-    <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
+    <ul class="exists">
+      <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
  </ul>
  <br><hr>
  <book-form @addBook='appendBook'></book-form>
@@ -13,28 +13,44 @@
 import BookItem from "./BookItem";
 import BookForm from "./BookForm";
 
+
 export default {
    name: "BookList",
   data() {
     return {
-      title: "All Books",
-      states: ["Want to Read", "Read", "Reading"],
+    title: "All Books",
+    states: ["Want to Read", "Read", "Reading"],
       books: [
-        { title: "Self-Reliance", author: "Ralph Waldo Emerson" },
-        { title: "American Gods", author: "Neil Gaiman" },
-        { title: "Amusing Ourselves to Death", author: "Neil Postman" }
-      ]
-    };
-  },
-  components: {
-    BookItem,
-    BookForm
-  },
-  methods: {
-    appendBook(bookTitle, bookAuthor) {
-      this.books.push({ title: bookTitle, author: bookAuthor });
-    }
+      {
+       title: "Self-Reliance",
+       author: "Ralph Waldo Emerson",
+       finishedReading: true
+     },
+     {
+       title: "American Gods",
+       author: "Neil Gaiman",
+       finishedReading: false
+     },
+     {
+       title: "Amusing Ourselves to Death",
+       author: "Neil Postman",
+       finishedReading: true
+     }
+   ]
+ };
+},
+components: {
+ BookItem,
+ BookForm
+},
+appendBook(bookData) {
+    this.books.push({
+      title: bookData.bookTitle,
+      author: bookData.bookAuthor,
+      finishedReading: bookData.finishedReading
+    });
   }
+}
 };
 </script>
 
