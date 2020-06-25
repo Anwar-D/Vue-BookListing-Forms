cbadd7ed7b899c6a72e38324100a60038c2284d8
Anwar <anyes202@hotmail.com>
2020-06-26 01:46:12 +0300

    task1

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,34 +1,47 @@
 <template>
-  <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" class="" action="#" method="post">
-    <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
-    <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+  <<form v-on:submit.prevent="bookSubmit(bookData)" class="" action="#" method="post">
+    <input v-model="bookData.bookTitle" type="text" name="title" value="" placeholder="Book Title">
+    <input v-model="bookData.bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+    <div>
+      <div>
+        <label for="finishedReading">
+          Finished Reading
+          <input v-model="bookData.finishedReading" type="checkbox" name="finishedReading" value="">
+        </label>
+      </div>
+    </div>
+    <div>
+      <label for="borrowed">
+        borrowed
+        <input v-model="bookData.ownership" type="radio" id="borrowed" value="borrowed">
+      </label>
+      <label for="bought">
+        bought
+        <input v-model="bookData.ownership" type="radio" id="bought" value="bought">
+      </label>
+    </div>
     <button type="submit" name="button">Add Book</button>
-  </form>
+ </form>
 </template>
-
-<script>
-export default {
-  name: "BookForm",
-  props: ["books"],
-  data() {
-  bookData: {
-        bookTitle: "",
-        bookAuthor: "",
-        finishedReading: false,
-        ownership: []
-      }
-    return {
-      bookTitle: "",
-      bookAuthor: ""
-    };
-  },
-  methods: {
+@@ -12,13 +30,17 @@ export default {
+ props: ["books"],
+ data() {
+   return {
+     bookTitle: "",
+     bookAuthor: ""
+     bookData: {
+       bookTitle: "",
+       bookAuthor: "",
+       finishedReading: false,
+       ownership: []
+     }
+   };
+ },
+ methods: {
     bookSubmit(bookTitle, bookAuthor) {
       this.$emit("addBook", bookTitle, bookAuthor);
+    bookSubmit(bookData) {
+      this.$emit("addBook", bookData);
     }
   }
 };
-</script>
-
-<style>
-</style>
