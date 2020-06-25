9208a89a73fe9851dd9ee729b0e12a53937da296
Anwar <anyes202@hotmail.com>
2020-06-26 01:54:45 +0300

    task2

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,11 +2,11 @@
   <div>
     <h1>{{title}}</h1>
     <ul>
-      <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
-    </ul>
-    <br><hr>
-    <book-form @addBook='appendBook'></book-form>
-  </div>
+    <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
+ </ul>
+ <br><hr>
+ <book-form @addBook='appendBook'></book-form>
+</div>
 </template>
 
 <script>
@@ -14,7 +14,7 @@ import BookItem from "./BookItem";
 import BookForm from "./BookForm";
 
 export default {
-  name: "BookList",
+   name: "BookList",
   data() {
     return {
       title: "All Books",
