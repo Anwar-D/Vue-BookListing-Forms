42db411ca9bcb7b7f6e748ce7d6aca9d7c35a523
Anwar <anyes202@hotmail.com>
2020-06-27 01:48:42 +0300

    task4

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,20 +1,21 @@
 <template>
 <div>
-  <h1>{{title}}</h1>
-  <ul class="exists">
-    <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
-  </ul>
-  <hr>
-  <h2>Filtered Books by Ownership</h2>
-  <select v-model="holding">
-    <option v-for="filter in filters">{{ filter }}</option>
-  </select>
-  <ul class="exists">
-    <book-item v-for='book in filteredBooks' :key='book.id' :book='book'></book-item>
-  </ul>
-  <br><hr>
-  <book-form @addBook='appendBook'></book-form>
-</div>
+    <h1>{{title}}</h1>
+    <input type="text" v-model="searchInput" placeholder="Search Books">
+    <ul class="exists">
+      <book-item v-for='book in searchedBooks' :key='book.id' :book='book'></book-item>
+    </ul>
+    <hr>
+    <h2>Filtered Books by Ownership</h2>
+    <select v-model="holding">
+      <option v-for="filter in filters">{{ filter }}</option>
+    </select>
+    <ul class="exists">
+      <book-item v-for='book in filteredBooks' :key='book.id' :book='book'></book-item>
+    </ul>
+    <br><hr>
+    <book-form @addBook='appendBook'></book-form>
+  </div>
 </template>
 
 <script>
@@ -23,44 +24,51 @@ import BookItem from "./BookItem";
 import BookForm from "./BookForm";
 export default {
 name: "BookList",
- data() {
-   return {
-   title: "All Books",
-     states: ["Want to Read", "Read", "Reading"],
-     books: [
-     {
-          title: "Self-Reliance",
-          author: "Ralph Waldo Emerson",
-          finishedReading: true,
-          ownership: "borrowed"
-        },
-        {
-          title: "American Gods",
-          author: "Neil Gaiman",
-          finishedReading: false,
-          ownership: "bought"
-        },
-        {
-          title: "Amusing Ourselves to Death",
-          author: "Neil Postman",
-          finishedReading: true,
-          ownership: "borrowed"
-        }
-      ],
-      filters: ["bought", "borrowed"],
-      holding: "bought"
-    };
+  data() {
+    return {
+    title: "All Books",
+      states: ["Want to Read", "Read", "Reading"],
+      books: [
+      {
+         title: "Self-Reliance",
+         author: "Ralph Waldo Emerson",
+         finishedReading: true,
+         ownership: "borrowed"
+       },
+       {
+         title: "American Gods",
+         author: "Neil Gaiman",
+         finishedReading: false,
+         ownership: "bought"
+       },
+       {
+         title: "Amusing Ourselves to Death",
+         author: "Neil Postman",
+         finishedReading: true,
+         ownership: "borrowed"
+       }
+     ],
+     filters: ["bought", "borrowed"],
+     holding: "bought",
+     searchInput: ""
+   };
+ },
+ components: {
+   BookItem,
+   BookForm
   },
-  components: {
-    BookItem,
-    BookForm
- },
- computed: {
-   filteredBooks() {
-     return _.filter(this.books, ["ownership", this.holding]);
-   }
- },
- methods: {
+  computed: {
+    filteredBooks() {
+      return _.filter(this.books, ["ownership", this.holding]);
+    },
+    searchedBooks() {
+      const searchFilter = book => {
+        return book.title.toLowerCase().match(this.searchInput.toLowerCase());
+      };
+      return _.filter(this.books, searchFilter);
+    }
+  },
+  methods: {
    appendBook(bookData) {
      this.books.push({
        title: bookData.bookTitle,
@@ -80,10 +88,10 @@ h2 {
 }
 ul {
 list-style-type: none;
- padding: 0;
+  padding: 0;
 }
 li {
- display: block;
- margin: 0 10px;
+  display: block;
+  margin: 0 10px;
 }
 </style>
